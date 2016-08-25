d482b750563ccb82e827ba4a324fd51950fe9475
Hallvard Traetteberg <hal@idi.ntnu.no>
2016-08-25 09:42:30 +0200

    Added test for LatLong.

---
+++ no.hal.pgo/no.hal.pgo.osm.tests/src/no/hal/pgo/osm/geoutil/LatLongTest.java
@@ -0,0 +1,24 @@
+package no.hal.pgo.osm.geoutil;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LatLongTest {
+
+	@Test
+	public void testLatLong() {
+		LatLong latLong = new LatLong(Math.PI, Math.E);
+		testPiE(latLong);
+	}
+
+	protected void testPiE(LatLong latLong) {
+		Assert.assertEquals(Math.PI, latLong.latitude, 0.0);
+		Assert.assertEquals(Math.E, latLong.longitude, 0.0);
+	}
+
+	@Test
+	public void testValueOf() {
+		LatLong latLong = LatLong.valueOf(Math.PI + ";" + Math.E, ";");
+		testPiE(latLong);
+	}
+}
